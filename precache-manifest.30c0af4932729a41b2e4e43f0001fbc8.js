4bd00c9260c102373327b0b58fd885bccbf4e0e5
Shashank  Shahare <srshahare22@gmail.com>
2020-03-29 00:42:47 -0700

    Updates

---
+++ precache-manifest.30c0af4932729a41b2e4e43f0001fbc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc60cf1c5988fe44ef732d91f81ce7ef",
+    "url": "/BurgerBuilder_react/index.html"
+  },
+  {
+    "revision": "476488c686781de1a76a",
+    "url": "/BurgerBuilder_react/static/css/main.f1996f77.chunk.css"
+  },
+  {
+    "revision": "8b362b5b92e0886532e7",
+    "url": "/BurgerBuilder_react/static/js/2.07bd6156.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BurgerBuilder_react/static/js/2.07bd6156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "476488c686781de1a76a",
+    "url": "/BurgerBuilder_react/static/js/main.26c48298.chunk.js"
+  },
+  {
+    "revision": "9dd31927f755f4ef4408",
+    "url": "/BurgerBuilder_react/static/js/runtime-main.5bafdc00.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilder_react/static/media/burger-logo.b8503d26.png"
+  }
+]);
